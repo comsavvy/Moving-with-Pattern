526fe76b41546a8fc93408cfe228fc0096299c2d
comsavvy <olusolaogundepo@gmail.com>
2020-09-30 16:02:15 +0100

    First edition of moving a pattern files

---
+++ moving_files.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import shutil as sh
+import sys
+from collections import namedtuple
+from glob import glob
+import re
+from pathlib import Path
+
+class WrongInputs(Exception):
+        pass
+
+class Movement:
+        def __init__(self, pattern, to):        
+                self.to = to
+                self.pattern = pattern
+
+        
+        def memory_size(self, place='to'):
+                '''
+                Returning the disk usage of the current directory
+                '''
+                try:
+                        if place.startswith('t'):
+                                return sh.disk_usage(self.to)
+                        elif place.startswith('f'):
+                                return sh.disk_usage('.')
+                except:
+                        print('The parameter is not well placed')
+
+                        
+        def memo_in_percent(self):
+                '''
+                Converting the used and free space to the overall percentage
+                
+                '''
+                print("Size of which location? ['from' | 'to']\n>>>>", end=' ')
+                place = input().lower()
+                space = self.memory_size(place)
+                total = 100
+                free = round(space.free/space.total*total, 2)
+                used = round(space.used/space.total*total, 2)
+                memory = namedtuple('Memory', ['Free', 'Used', 'Total'])
+                return memory(free, used, total)
+
+        
+        def move(self):
+                '''
+                Moving the capture pattern to the specified location
+                '''
+                try:
+                        data = glob(self.pattern)
+                        for i in data:
+                                sh.move(i, self.to)
+                except Exception as e:
+                        print(f'The process cannot be done because {e}')
+
+def main():
+        try:
+                path1 = input('Input the pattern to use: ')
+                path2 = input('The location to move-to: ')
+                mov = Movement(path1, path2)
+                print('Do you want to move the data to the specified location?\n[Y|N]>>>', end=' ')
+                ask = input().capitalize()
+                if ask.startswith('Y'):
+                        mov.move()
+                print('Do you want to check the memory usage?\n[Y|N]>>>', end=' ')
+                ask = input().capitalize()
+                if ask.startswith('Y'):
+                        print(mov.memo_in_percent())
+                else:
+                        pass
+                sys.exit(0)
+        except Exception as e:
+                raise WrongInputs('Invalid pattern or location')
+                sys.exit(1)
+                
+if __name__ == '__main__':
+        main()
