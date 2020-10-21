f67680e27c956c6975840ed5e509f224e8976ec1
comsavvy <olusolaogundepo@gmail.com>
2020-10-21 14:26:11 +0100

    Dataclass was introduced and blank line was treated [success]

--- moving_files.py
+++ moving_files.py
@@ -4,39 +4,41 @@ import sys
 from collections import namedtuple
 from glob import glob
 import os
+from dataclasses import dataclass
+
 
 class WrongInputs(Exception):
-        pass
+    pass
 
+
+@dataclass
 class Movement:
-    '''
+    """
+    Movement(from_, pattern, to)
+
     Moving files through pattern from one location to the other
     And viewing the size through the location specify has taken in the memory
-
-    '''
-    def __init__(self, pattern, to, from_):
-        self.to = to
-        self.pattern = pattern
-        self.from_ = from_
+    """
+    from_: str
+    pattern: str
+    to: str
 
     def memory_size(self, place='to'):
-        '''
+        """
         Returning the disk usage of the current directory
-        '''
+        """
         try:
             if place.startswith('t'):
                 return sh.disk_usage(self.to)
             elif place.startswith('f'):
                 return sh.disk_usage('.')
-        except:
-            WrongInputs('The parameter is not well placed')
-
+        except Exception as e:
+            WrongInputs(f'The parameter is not well placed\n{e}')
 
     def memo_in_percent(self):
-        '''
+        """
         Converting the used and free space to the overall percentage
-
-        '''
+        """
         print("Size of which location? ['from' | 'to']\n>>>>", end=' ')
         place = input().lower()
         space = self.memory_size(place)
@@ -46,17 +48,17 @@ class Movement:
         memory = namedtuple('Memory', ['Free', 'Used', 'Total'])
         return memory(free, used, total)
 
-
     def move(self):
-        '''
+        """
         Moving the capture pattern to the specified location
-        '''
+        """
         try:
             data = glob(os.path.join(self.from_, self.pattern))
             for i in data:
-                    sh.move(i, self.to)
+                sh.move(i, self.to)
         except Exception as e:
-            print(f'The process cannot be done because {e}')
+            print(f'{e}')
+
 
 def main():
     try:
@@ -64,21 +66,22 @@ def main():
         path0 = input('>>> ')
         path1 = input('Input the pattern to use: ')
         path2 = input('The location to move-to: ')
-        mov = Movement(path1, path2, path0)
+        mov = Movement(path0, path1, path2)
         print('Do you want to move the data to the specified location?\n[Y|N]>>>', end=' ')
         ask = input().capitalize()
         if ask.startswith('Y'):
-                mov.move()
+            mov.move()
         print('Do you want to check the memory usage?\n[Y|N]>>>', end=' ')
         ask = input().capitalize()
         if ask.startswith('Y'):
-                print(mov.memo_in_percent())
+            print(mov.memo_in_percent())
         else:
-                pass
+            pass
         sys.exit(0)
     except Exception as e:
-        raise WrongInputs('Invalid pattern or location')
+        raise WrongInputs(f'Invalid pattern or location')
         sys.exit(1)
+
 
 if __name__ == '__main__':
     main()
